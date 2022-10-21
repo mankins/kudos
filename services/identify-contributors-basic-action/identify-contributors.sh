d3d934cf1c0c943d99837cf5fc63f0917c8faedd
Matt Mankins <mankins@gmail.com>
2022-10-21 17:55:03 +0200

    debug

--- services/identify-contributors-basic-action/identify-contributors.sh
+++ services/identify-contributors-basic-action/identify-contributors.sh
@@ -8,6 +8,8 @@ rm /tmp/kudos.txt
 
 echo "nodejs..."
 
+find .
+
 # try contributors
 find . -type f -name 'package.json' -prune | while read line; do cat "$line" | jq -r '.contributors[].email' | grep -v null >> /tmp/kudos.txt; done
 
