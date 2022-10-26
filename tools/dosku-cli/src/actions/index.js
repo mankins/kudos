1272b7f4b96d7c10ca79c44ea90acf3e93749f1e
Matt Mankins <mankins@gmail.com>
2022-10-26 20:00:51 +0200

    feature(dosku): add identify proxy

--- tools/dosku-cli/src/actions/index.js
+++ tools/dosku-cli/src/actions/index.js
@@ -1,9 +1,10 @@
 // local actions
 
 import * as config from "./config.js";
+import * as identify from "./identify.js";
 import * as ink from "./ink.js";
 import * as install from "./install.js";
 import * as list from "./list.js";
 import * as run from "./run.js";
 
-export { config, ink, install, list, run };
+export { config, identify, ink, install, list, run };
